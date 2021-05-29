4a9d23d2b76734160e76af24bf5c2d62b333da6c
Emre FIRAT <emrefirat93@outlook.com>
2021-05-29 18:39:18 +0300

    Swagger2 desteği getirildi.

--- src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
+++ src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -1,31 +1,23 @@
 package kodlamaio.hrms.entities.concretes;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name="job_titles")
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobTitle {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private int id;
 
     @Column(name="name")
     private String name;
-
-    public JobTitle() {
-    }
-
-    public JobTitle(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
 
